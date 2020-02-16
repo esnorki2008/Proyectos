26822848be0dfb4475ae114c8d483eeace602855
AndhySolis <49577095+AndhySolis@users.noreply.github.com>
2020-02-15 22:36:06 -0500

    1
    1

--- Archivos/HojaDeTrabajo1/HojaDeTrabajo1.pro
+++ Archivos/HojaDeTrabajo1/HojaDeTrabajo1.pro
@@ -1,7 +1,8 @@
 QT -= gui
 
 CONFIG += c++11 console
-CONFIG -= app_bundle
+CONFIG -= app_bundle executable
+
 
 # The following define makes your compiler emit warnings if you use
 # any Qt feature that has been marked deprecated (the exact warnings
@@ -13,7 +14,7 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # In order to do so, uncomment the following line.
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
-
+INSTALLS +=executable
 SOURCES += \
         main.cpp
 
--- Archivos/HojaDeTrabajo1/main.cpp
+++ Archivos/HojaDeTrabajo1/main.cpp
@@ -23,10 +23,39 @@ void MostrarTodo();
 int main()
 {
     CrearArchivo();
-    //Agregar();
+    int Entrada=1;
+    while (Entrada!=4) {
+        std::cout<<"Hoja De Trabajo 1  201700886"<<std::endl;
+        std::cout<<"***********************************************"<<std::endl;
+        std::cout<<"Escriba El Numero Para La Accion:"<<std::endl<<"1_Agregar,2_Actualizar,3_MostrarTodo,4_Salir"<<std::endl;
+        std::cout<<"***********************************************"<<std::endl;
+        std::cin>>Entrada;
+        switch (Entrada) {
+        case 1:{
+            Agregar();
+            break;
+        }
+        case 2:{
+            Actualizar();
+            break;
+        }
+        case 3:{
+            MostrarTodo();
+            break;
+        }
+        default:{
+            Entrada=4;
+            std::cout<<"Saliendo"<<std::endl;
+            Entrada=4;
+            break;
+        }
 
-    Actualizar();
-    MostrarTodo();
+        }
+
+
+
+    }
+
     //QCoreApplication a(argc, argv);
     return 0;
 }
@@ -40,8 +69,9 @@ void MostrarTodo(){
     std::cout<<"Información De Todos Los Alumnos Y Profesores"<<std::endl;;
     while (!feof(file)) // Leer Al Fin
     {
-        fread(&Info, sizeof(Str), 1, file);
+
         if(!feof(file)){
+        fread(&Info, sizeof(Str), 1, file);
         std::cout<<"***********************************************"<<std::endl;
         std::cout<<"Tipo: "<<Info.Tipo<<std::endl
                 <<"Apellido1: "<<Info.Apellido1<<std::endl
@@ -67,7 +97,6 @@ void Actualizar(){
     file = fopen("alumnos_[201700886]", "r+");
     fseek(file, Indice, SEEK_SET );
     fread(&Info, sizeof(Str), 1, file);
-
     std::cout<<"Ingrese La Información Que Se Solicita Para Actualizar"<<std::endl;;
 
     //std::cout<<"Ingrese El Tipo (0 Estudiante| 1 Profesor)"<<std::endl;
@@ -87,8 +116,10 @@ void Actualizar(){
     else
         std::cout<<"Ingrese Seccion"<<std::endl;
     std::cin >>Info.Nota;
+    fseek(file, Indice, SEEK_SET ); 
+    fwrite(&Info, sizeof(Str), 1, file);
     fclose(file);
-    Escribir(&Info);
+
     std::cout<<"Se Ha Actualizado La Información"<<std::endl;
 }
 void Escribir(Str *Info){
@@ -154,18 +185,21 @@ int BuscarStruct(const char *Entrada){
     file = fopen("alumnos_[201700886]", "r+");
     Str Dato;
     //
-    int Seek=-1;
+    int Seek=0;
     while (!feof(file)) // Leer Al Fin
     {
         fread(&Dato, sizeof(Str), 1, file);
-        Seek=Seek+(int)sizeof(Str);
+        if(feof(file))
+            return -1;
+
         if(CompararChar(Dato.Codigo,Entrada)){
             return Seek;
         }
+        Seek=Seek+(int)sizeof(Str);
 
     }
     fclose(file);
-    return Seek;
+    return -1;
 }
 void CrearArchivo(){
     std::ifstream infile("alumnos_[201700886]");
