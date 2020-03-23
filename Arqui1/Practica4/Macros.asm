d95facd848981be6b94a667c997b3afaa8536df3
esnorki2008 <49577095+esnorki2008@users.noreply.github.com>
2020-03-23 17:27:06 -0600

    Colocar Tablero

--- Arqui1/Practica4/Macros.asm
+++ Arqui1/Practica4/Macros.asm
@@ -1,19 +1,208 @@
 Print macro Texto
+   xor ax,ax
    mov   ax, data     ;hmm ¿seg?
    mov   ds,ax          ;ds = ax = saludo
    mov   ah,09          ;Function(print string)
    mov   dx,Offset Texto         ;DX = String terminated by "$"
    int   21h               ;Interruptions DOS Functions
 endm
+Val0 macro Char
+;65  72
+    local salir
+    cmp Char,65    
+    mov dl,00h
+    jz salir
+    cmp Char,66
+    mov dl,01h
+    jz salir
+    cmp Char,67
+    mov dl,02h
+    jz salir
+    cmp Char,68
+    mov dl,03h
+    jz salir
+    cmp Char,69
+    mov dl,04h
+    jz salir
+    cmp Char,70
+    mov dl,05h
+    jz salir
+    cmp Char,71
+    mov dl,06h
+    jz salir
+    ;Ultimo O No Reconocido
+    mov dl,07h    
+    salir:
+endm
+Val1 macro Char
+;49  57
+    local salir
+    cmp Char,49    
+    mov dl,07h
+    jz salir
+    cmp Char,50
+    mov dl,06h
+    jz salir
+    cmp Char,51
+    mov dl,05h
+    jz salir
+    cmp Char,52
+    mov dl,04h
+    jz salir
+    cmp Char,53
+    mov dl,03h
+    jz salir
+    cmp Char,54
+    mov dl,02h
+    jz salir
+    cmp Char,55
+    mov dl,01h
+    jz salir
+    ;Ultimo O No Reconocido
+    mov dl,00h    
+    salir:
+endm
 PrintN macro Num
+    xor ax,ax
     mov dl,Num ; print 'A'
     mov ah,06h
     add dl,30h
     int 21h
 endm
+PrintArr macro Arreglo
+    local lup
+    mov si,00h
+    mov bl,5h
+    lup:
+    mov dl,Arreglo[si]
+    mov ah,2
+    int 21h
+    inc si
+    dec bl
+    jnz lup
+endm
+
+Comparar macro Texto1,Texto2
+local iniciocompa,correcto,incorrecto,fincompa
+mov si,0h
+mov al,5h
+iniciocompa:
 
 
+mov BL,Texto1[si]
+mov BH,Texto2[si]
 
+
+cmp BL,BH
+jnz incorrecto;No Es Igual
+
+inc si
+dec al
+jnz iniciocompa
+jmp correcto;Todos Son Iguales
+
+
+incorrecto:
+xor bx,bx
+mov bl,35
+jmp fincompa
+correcto:
+xor bx,bx
+mov bl,64
+jmp fincompa
+fincompa:
+push bx
+endm
+
+Entrada macro
+local Input,FININPUT,reinicio
+mov si,00h
+mov bl,5h
+reinicio:
+mov Teclado[si],0h
+inc si
+dec bl
+jnz reinicio
+mov Teclado[si],0h
+
+mov bl,5h
+mov si,00h
+mov ah,1;Entrada en al
+Input:
+int 21h
+
+cmp al,13
+JZ FININPUT
+mov Teclado[si],al
+
+dec bl
+jz FININPUT
+
+inc si
+Jnz Input
+FININPUT:
+
+
+mov ax,si
+mov Longitud,al
+xor si,si
+xor ax,ax
+xor dx,dx
+endm
+
+NuevaLinea macro
+MOV dl, 10
+MOV ah, 02h
+INT 21h
+MOV dl, 13
+MOV ah, 02h
+INT 21h
+endm
+Espacio macro
+MOV dl, 32
+MOV ah, 02h
+INT 21h
+endm
+
+DivX macro
+MOV dl, 45
+MOV ah, 02h
+INT 21h
+MOV dl, 45
+MOV ah, 02h
+INT 21h
+MOV dl, 45
+MOV ah, 02h
+INT 21h
+endm
+
+F0 macro
+MOV dl, 32
+MOV ah, 02h
+INT 21h
+MOV dl, 32
+MOV ah, 02h
+INT 21h
+
+endm
+
+
+PrintJ macro Num
+cmp Num,1h    
+jz Jug1
+jc SinJug
+Jnc Jug2
+SinJug:
+Print J0
+jmp SalSelec
+Jug1:
+Print J1
+jmp SalSelec
+Jug2:
+Print J2
+jmp SalSelec
+SalSelec:
+endm
 
 Exit macro
     mov   ax,4c00h       ;Function (Quit with exit code (EXIT))
--- Arqui1/Practica4/P4.asm
+++ Arqui1/Practica4/P4.asm
@@ -1,145 +1,245 @@
 include Macros.asm ; archivo con los macros
 
 .model small
-.stack 100h
+.stack 500h
 .data
    ;A1 db 1,2,3,4,5,6,7,8
    
 
-Titulo1  db "UNIVERSIDAD DE SAN CARLOS DE GUATEMALA",10,13,"FACULTAD DE INGENIERIA","CIENCIAS Y SISTEMAS",10,13,"$"    
+Titulo1  db "UNIVERSIDAD DE SAN CARLOS DE GUATEMALA",10,13,"$"
+;Titulo1  db "UNIVERSIDAD DE SAN CARLOS DE GUATEMALA",10,13,"FACULTAD DE INGENIERIA","CIENCIAS Y SISTEMAS",10,13,"$"    
 Titulo2 db "ARQUITECTURA DE COMPUTADORES Y ENSAMBLADORES 1",10,13,"NOMBRE: ANDHY LIZANDRO SOLIS OSORIO",10,13,"$"
-Tiutlo3 db "CARNET: 201602634",10,13,"SECCION: B",10,13,"$" 
-
+Titulo3 db "CARNET: 201700886",10,13,"SECCION: B",10,13,"$" 
+Ocupado db "La Casilla Esta Ocupada","$" 
+Turno1 db "Turno BLANCAS","$"
+Turno2 db "Turno NEGRAS","$"
 
 Fila1 db 32,32,32,32,45,45,45,32,32,45,45,45,32,32,45,45,45,32,32,45,45,45,32,32,45,45,45,32,32,45,45,45,32,32,45,45,45,32,32,45,45,45,32,32,"$"
-Fila0 db 32,32,179,32,32,32,32,179,32,32,32,32,179,32,32,32,32,179,32,32,32,32,179,32,32,32,32,179,32,32,32,32,179,32,32,32,32,179,32,32,32,32,32,179,"$"
+Fila0 db 32,32,179,32,32,32,32,179,32,32,32,32,179,32,32,32,32,179,32,32,32,32,179,32,32,32,32,179,32,32,32,32,179,32,32,32,32,179,"$"
+FilaF0 db 32,32,"A",32,32,32,32,"B",32,32,32,32,"C",32,32,32,32,"D",32,32,32,32,"E",32,32,32,32,"F",32,32,32,32,"G",32,32,32,32,"H","$"
 
-Tabulador db 09,"$"
-NuevaLinea db 10,13,"$"
-Espacio db 32,"$"
+
 J0 db 32,32,"$"
 J1 db "FB","$"
 J2 db "FN","$" 
 DibX db "---","$"
 DibY db "|","$"
 
-IniMenu   db "Menu", "$"
-    
-    
-   
-    
-   
-   Lectura db ?
-   file db 'c:\arc.txt','00h' ;ojo con el 00h es importante
-   handler dw ?
-   buffer db 10 dup(' ')
-Tablero db 81 dup(0h)
-   
-   PosX db 0
-   PosY db 0
-   
-   Registro db 0  
-   
+Menu0   db "Menu Del Juego", "$"
+Menu1 db "1) IniciarJuego", "$"
+Menu2 db "2) CargarJuego", "$"
+Menu3 db "3) Salir", "$" 
+              
+Lectura db ?
+file db 'c:\arc.txt','00h' ;ojo con el 00h es importante
+handler dw ?
+buffer db 10 dup(' ')
+Tablero db 64 dup(5)   
+PosX db 0
+PosY db 0
+Modo db 0;Modo 0 Menu Principal 
+Registro db 0  
+Estado db 0 
+b0 db 20 dup("$")
+
+Teclado db 20 dup("$")
+Longitud db 0
+
+Array1 db 5 dup("$")
+Array2 db 5 dup("$")
+
+b1 db 20 dup("$")
+;Palabras Reservadas
+
+Jugador db 0
+Pass db 6 dup(0h)
+
+
+Tiro db 100 dup(0h)
 .code
 
 
 
-main  proc   
-jmp hola
+main  proc
+;jmp InicioTablero
+
+;Iniciar Palabras Reservadas
+
+;pass
+mov Pass[0h],112
+mov Pass[1h],97
+mov Pass[2h],115
+mov Pass[3h],115
+mov Pass[4h],0
+Print Titulo1
+Print Titulo2
+Print Titulo3
+NuevaLinea
+Menu:
+;Imprimir Menu
+Print Menu0
+NuevaLinea
+Print Menu1
+NuevaLinea
+Print Menu2
+NuevaLinea
+Print Menu3
+NuevaLinea
+
+Entrada
+
+;NuevaLinea
+;PrintArr Teclado
+;NuevaLinea
+;PrintArr Uno
+;NuevaLinea
+
+ 
+cmp Teclado[0h],49
+jz InicioTablero
+cmp Teclado[0h],50
+jz Cargar
+cmp Teclado[0h],51
+jz Fin 
+
+jmp Menu:
+    
 
 
-mov ah,81;Iniciar Tablero
-mov si,offset Tablero
+;pop BX
+;cmp Bl,64
+;mov dl,Bl
+;mov ah,2
+;int 21h
+
+;jmp Fin
+
+
+
+InicioTablero:  ;INICIO DE DIBUJAR TABLERO
+mov bh,64;Iniciar Tablero
+mov si,00h
 Iniciar:
-mov [si],0h
+mov al,0h
+mov Tablero[si],al
 inc si
-dec ah 
+dec bh
 jnz Iniciar
+mov Jugador,1h ;Empezando Con Jugador1 
+JuegoTablero:
+NuevaLinea
+cmp Jugador,1h
+jz TJ1
+jnz TJ2
+TJ1:
+print Turno1
+jmp FinTJ
+TJ2:
+print Turno2
+FinTJ:
+NuevaLinea
 
-
-
-hola:
-
-mov si,offset Tablero
-inc si
-mov PosY,9h
-mov bl,PosY       
-Print NuevaLinea
-mov PosY,bl
-
-
-
-TabY:
-
-PrintN PosY
-Print Espacio
            
 
 
-
-Print J1
-Print DibX
-inc si  
+mov si,000h
+mov PosY,8
 
 
-Print J1
-Print DibX
+TablaY:
+
+mov PosX,8
+PrintN PosY
+Espacio
+
+
+
+TablaX:
+PrintJ Tablero[si]
 inc si
 
-Print J1
-Print DibX
-inc si
-
-Print J1
-Print DibX
-inc si
-
-Print J1
-Print DibX
-inc si
-
-Print J1
-Print DibX
-inc si
-
-Print J1
-Print DibX
-inc si
-
-Print J1
-Print DibX
-inc si
-
-Print J1
-inc si
+dec PosX
+jz FinTableroX
+DivX
 
 
+FinTableroX:
+jnz TablaX
 
 
+NuevaLinea
+
+mov bl,PosY
+dec bl
+jz FinTableroY
+
+
+Print Fila0
+jmp FinalConte
+FinTableroY:
+NuevaLinea
+Print FilaF0
+NuevaLinea
+
+FinalConte:
 dec PosY
-jz FinTablero
-Print NuevaLinea 
-Print Fila0           
-Print NuevaLinea
-FinTablero:    
-jnz TabY
+NuevaLinea
+jnz TablaY 
 
-  
-  
 
-   
-   
-           ;Inicia proceso
-;fopen file, handler
-;fread 10,buffer,handler
-;fclose handler ; siempre se debe cerrar un archivo
-;ov   ax, @data     ;hmm ¿seg?
-;mov   ds,ax          ;ds = ax = saludo
-
-   
+Entrada
 
 
 
+
+;Val0 Teclado[0]
+Val1 Teclado[1]
+mov Registro,dl
+mov al,8h
+mul Registro
+mov Registro,al
+;Val1 Teclado[1]
+Val0 Teclado[0]
+add dl,Registro
+xor dh,dh
+
+;mov ah,2
+;int 21h
+
+
+;mov dl,Teclado[1]
+;Val Teclado[1]
+;mov ah,2
+;int 21h
+
+mov si,dx
+mov al,Tablero[si]
+mov Registro,al
+cmp Registro,0h
+jnz NoColocar
+Colocar: 
+mov al,Jugador
+mov Tablero[si],al
+
+;CAMBIAR DE JUGADOR
+cmp Jugador,1h
+jz CJ1
+Jnz Cj2
+CJ1:
+mov Jugador,2h
+jmp CJF
+CJ2:
+mov Jugador,1h
+CJF:
+jmp JuegoTablero
+
+NoColocar:
+Print Ocupado 
+Entrada
+jmp JuegoTablero
+
+Cargar:
+Fin:
 
 
 
