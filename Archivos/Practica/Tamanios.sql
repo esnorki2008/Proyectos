1419356eafe216973c7cfb9b38b91b36bc8a682e
AndhySolis <49577095+AndhySolis@users.noreply.github.com>
2020-04-07 10:24:45 -0600

    Inicial Proyecto 2

---
+++ Archivos/Practica/Tamanios.sql
@@ -0,0 +1,9 @@
+use Practica;
+select count(*) as TamanioLevel1A from Level_1A;
+select count(*) as TamanioProject from Project;
+select count(*) as TamanioTypeStatus from Type_Status;
+select count(*) as TamanioGeoname from Geoname;
+select count(*) as TamanioLocation from Location;
+select count(*) as TamanioCurrency from Currency;
+select count(*) as TamanioCountry_Code from Country_Code;
+select count(*) as TamanioTransaction from Transaction;
